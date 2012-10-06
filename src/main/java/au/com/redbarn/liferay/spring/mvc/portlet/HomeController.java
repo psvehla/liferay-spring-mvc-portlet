c1d27219989a0e9e46a9d5b5491a0162d05a24cd
psvehla <psvehla@hotmail.com>
2012-10-07 01:12:14 +1000

    Missed a bit.

---
+++ src/main/java/au/com/redbarn/liferay/spring/mvc/portlet/HomeController.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of liferay-spring-mvc-portlet.
+ *
+ * liferay-spring-mvc-portlet is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * liferay-spring-mvc-portlet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * liferay-spring-mvc-portlet. If not, see <http://www.gnu.org/licenses/>.
+ */
+package au.com.redbarn.liferay.spring.mvc.portlet;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+/**
+ * Handles requests for the view mode.
+ */
+@Controller
+@RequestMapping("VIEW")
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RenderMapping
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! the client locale is "+ locale.toString());
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate);
+		
+		return "home";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,6 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<h1>Liferay Spring Portlet MVC</h1>
+
+<p>The time on the server is ${serverTime}.</p>
+
