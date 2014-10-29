1d1c8d2d8a2de11e81082fe7521e13ba7bdc94d8
Peter Svehla <peter.svehla@permeance.com.au>
2014-10-29 14:48:45 +1100

    Changed log to Liferay one. More updates. More tidying.

--- src/main/java/au/com/redbarn/liferay/spring/mvc/portlet/HomeController.java
+++ src/main/java/au/com/redbarn/liferay/spring/mvc/portlet/HomeController.java
@@ -20,12 +20,13 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 /**
  * Handles requests for the view mode.
@@ -33,24 +34,25 @@ import org.springframework.web.portlet.bind.annotation.RenderMapping;
 @Controller
 @RequestMapping("VIEW")
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
+	private static final Log log = LogFactoryUtil.getLog(HomeController.class);
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RenderMapping
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! the client locale is "+ locale.toString());
-		
+
+		log.info("Welcome home! the client locale is "+ locale.toString());
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate);
-		
+
 		return "home";
 	}
-	
+
 }
